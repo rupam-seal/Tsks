312e908b202872407192e4a415f22c756e7f50af
rupam-seal <rupam.x.seal@gmail.com>
2022-07-30 06:16:33 +0530

    ✨frontend(Sidebar): added navigation dot

---
+++ frontend/src/components/sidebar/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div class="top__dot-container">
+      <div class="top__dot dot1"></div>
+      <div class="top__dot dot2"></div>
+      <div class="top__dot dot3"></div>
+    </div>
+  );
+};
+
+export default Navigation;
