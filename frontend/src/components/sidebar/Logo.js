defd32a7e6617ac9eb69bc4b2aa25b5254e4fc5f
rupam-seal <rupam.x.seal@gmail.com>
2022-07-31 06:18:12 +0530

    ✨frontend(Sidebar): added logo

---
+++ frontend/src/components/sidebar/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div class="top__logo-container">
+      <img src="" alt="" class="top__img-logo"></img>
+      <h3 class="top__txt-logo">
+        Ts<span class="top__highlight">ks.</span>
+      </h3>
+    </div>
+  );
+};
+
+export default Logo;
