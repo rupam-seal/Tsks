4998722bcf0efb25458c28755ac57d57ad825de7
rupam-seal <rupam.x.seal@gmail.com>
2022-08-07 11:12:02 +0530

    ♻️frontend(Sidebar): refractor code

--- frontend/src/components/sidebar/Label.js
+++ frontend/src/components/sidebar/Label.js
@@ -13,20 +13,30 @@ export const LabelList = () => {
   return (
     <div>
       <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
-      <LableItem>Read</LableItem>
+      <LableItem>Coding</LableItem>
+      <LableItem>Workout</LableItem>
+      <LableItem>Django</LableItem>
+      <LableItem>UX</LableItem>
+      <LableItem>Programming</LableItem>
+      <LableItem>Gym</LableItem>
+      <LableItem>Dinner</LableItem>
+      <LableItem>Workout</LableItem>
     </div>
   );
 };
 
 export const LableItem = ({ children }) => {
-  return <div className="label">{children}</div>;
+  const colors = ['#ff0077', '#c300ff', '#009dff', '#09ff00', '#ff8000'];
+  const random = Math.floor(Math.random() * colors.length);
+
+  return (
+    <div
+      style={{ backgroundColor: colors[random] + 26, color: colors[random] }}
+      className="label"
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Label;
