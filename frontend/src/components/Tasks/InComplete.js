5dad83d05c705b5497448b0203d89277f7e0a65a
rupam-seal <rupam.x.seal@gmail.com>
2022-08-25 11:17:53 +0530

    ✨frontend(Tasks): added Incomplete Card

---
+++ frontend/src/components/Tasks/InComplete.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ParentCard from './ParentCard';
+
+const InComplete = () => {
+  return (
+    <div className="incomplete">
+      <ParentCard />>
+    </div>
+  );
+};
+
+export default InComplete;
