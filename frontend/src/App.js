ba90706258decc8a8e9629afc064076ab18bbd3f
rupam-seal <rupam.x.seal@gmail.com>
2022-08-30 11:19:29 +0530

    ♻️frontend(*): refractor code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Sidebar } from './components/sidebar';
+import Tasks from './components/Tasks/Tasks';
+import Topbar from './components/topbar/Topbar';
 
 const App = () => {
-  return <Sidebar />;
+  return (
+    <>
+      <Sidebar />
+      <Topbar />
+      <Tasks />
+    </>
+  );
 };
 
 export default App;
